b0c78aa68551e446434af2491a9c17397fca08be
dabao <1213365465@qq.com>
2020-06-22 19:17:13 +0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import GameComp from './components/GameComp'
+import React from 'react';
+import GameComp from './components/GameComp';
 
 function App() {
   return (
     <div className="App">
       <GameComp />
+      <div>12345</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
